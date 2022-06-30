9b6f8e1dc2f7c4e45accc5d7a31e3db8429089fb
Mehadi Hasan <Mehadihasan.phy@gmail.com>
2022-06-30 16:10:05 +0600

    Calender added

--- src/components/Calender/Calender.js
+++ src/components/Calender/Calender.js
@@ -1,10 +1,17 @@
-
+import { DayPicker } from "react-day-picker";
+import "react-day-picker/dist/style.css";
 const Calender = () => {
-    return (
-        <div>
-            <h1 className="text-3xl font-mono text-center mt-20 mb-20 font-bold">Calender</h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-3xl text-black-500 font-mono text-center mt-20 mb-20 font-bold">
+        Calender
+      </h1>
+      <div className="card lg:card-side bg-base-100 shadow-xl justify-center w-9/12 mx-auto mt-20 mb-32">
+      <DayPicker />
+      </div>
+      
+    </div>
+  );
 };
 
 export default Calender;
--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     
     </>
     return (
-        <div class="navbar bg-base-100 font-mono font-bold px-12">
+        <div class="navbar bg-#01a3a4 font-mono font-bold px-12">
   <div class="navbar-start">
     <div class="dropdown">
       <label tabindex="0" class="btn btn-ghost lg:hidden">
